5007a9777861011225dc415815ab7ec8e75bad79
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-12-19 08:29:00 +0700

    Decompose MVVM for Sorting

---
+++ Sorting/Views/InputArrayLengthWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace ProgrammingThirdSem.Sorting.Views
+{
+    public partial class InputArrayLengthWindow : Window
+    {
+        public int? ArrayLength { get; private set; }
+        public int? MinValue { get; private set; }
+        public int? MaxValue { get; private set; }
+        
+        public InputArrayLengthWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(ArrayLengthTextBox.Text, out int length) && int.TryParse(MinValueTextBox.Text, out int minValue) && int.TryParse(MaxValueTextBox.Text, out int maxValue) && length > 0)
+            {
+                ArrayLength = length;
+                MinValue = minValue;
+                MaxValue = maxValue;
+                DialogResult = true; // Устанавливаем результат, чтобы закрыть окно
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное положительное число!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
--- Sorting/Views/SortingView.xaml.cs
+++ Sorting/Views/SortingView.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using System.Windows.Controls;
+using ProgrammingThirdSem.Sorting.ViewModels;
 
-namespace ProgrammingThirdSem
+namespace ProgrammingThirdSem.Sorting.Views
 {
     public partial class SortingView : UserControl
     {
         public SortingView()
         {
             InitializeComponent();
+            DataContext = new SortingViewModel();
         }
     }
 }
